a0ce449c295ae4853a577db77dafb5a9444356a4
dmitrijs lotko <dmitrijs.lotko@1nce.com>
2022-08-26 18:33:49 +0300

    added s3 support

--- lambda_layer.tf
+++ lambda_layer.tf
@@ -1,6 +1,9 @@
+locals {
+  s3_layer = var.bucket != null && var.key != null
+}
 resource "null_resource" "node_dependencies" {
   provisioner "local-exec" {
-    working_dir = var.package_directory
+    working_dir = var.layer_directory
     command     = "npm install"
   }
 
@@ -19,9 +22,18 @@ data "archive_file" "dependencies" {
   ]
 }
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename            = data.archive_file.dependencies.output_path
   layer_name          = var.layer_name
-  source_code_hash    = fileexists(data.archive_file.dependencies.output_path) ? filebase64sha256(data.archive_file.dependencies.output_path) : data.archive_file.dependencies.output_base64sha256
+  source_code_hash    = local.s3_layer ? aws_s3_object.object[0].etag : fileexists(data.archive_file.dependencies.output_path) ? filebase64sha256(data.archive_file.dependencies.output_path) : data.archive_file.dependencies.output_base64sha256
   compatible_runtimes = var.compatible_runtimes
   skip_destroy        = var.skip_destroy
+  s3_bucket           = local.s3_layer ? aws_s3_object.object[0].bucket : null
+  s3_key              = local.s3_layer ? aws_s3_object.object[0].key : null
+  filename            = local.s3_layer ? null : data.archive_file.dependencies.output_path
+}
+
+resource "aws_s3_object" "object" {
+  count  = local.s3_layer ? 1 : 0
+  bucket = var.bucket
+  key    = var.key
+  source = data.archive_file.dependencies.output_path
 }
